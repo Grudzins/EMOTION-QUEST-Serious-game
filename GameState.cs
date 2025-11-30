a0da2e3c49b38f8df9e94154d4043bd81e031c58
Grudzins <163037794+Grudzins@users.noreply.github.com>
2025-11-30 19:09:14 +0100

    Update GameState.cs

--- GameState.cs
+++ GameState.cs
@@ -1,1 +1,36 @@
+using System;
 
+namespace EmotionQuest.Models
+{
+    public class GameState
+    {
+        public int Score { get; private set; }
+        public int RoundsPlayed { get; private set; }
+        public int CorrectAnswers { get; private set; }
+        public string Difficulty { get; private set; } = "Łatwy";
+
+        public double Accuracy => RoundsPlayed == 0
+            ? 0
+            : (double)CorrectAnswers / RoundsPlayed;
+
+        public void Update(bool correct)
+        {
+            RoundsPlayed++;
+
+            if (correct)
+            {
+                CorrectAnswers++;
+                Score += 5;
+            }
+            else
+            {
+                Score -= 2;
+            }
+        }
+
+        public void SetDifficulty(string difficulty)
+        {
+            Difficulty = difficulty;
+        }
+    }
+}
