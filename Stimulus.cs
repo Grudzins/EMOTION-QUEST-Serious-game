b3ee6806cfd55da722e3b32bb9576cb93d4c54dc
Grudzins <163037794+Grudzins@users.noreply.github.com>
2025-11-30 19:10:15 +0100

    Update Stimulus.cs

--- Stimulus.cs
+++ Stimulus.cs
@@ -1,1 +1,9 @@
-
+namespace EmotionQuest.Models
+{
+    public class Stimulus
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }       // opis sytuacji
+        public string CorrectEmotion { get; set; }    // np. "Radość", "Smutek"
+    }
+}
